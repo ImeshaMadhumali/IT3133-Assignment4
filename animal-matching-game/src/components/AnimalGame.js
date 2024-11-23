45a563a01a73199a959f23047d9b5ee71877fb23
Imesha Madhumali <139534200+ImeshaMadhumali@users.noreply.github.com>
2024-11-23 14:50:56 +0000

    Finalize the app

--- animal-matching-game/src/components/AnimalGame.js
+++ animal-matching-game/src/components/AnimalGame.js
@@ -3,13 +3,11 @@ import AnimalCard from './AnimalCard';
 import { animals } from '../assets/data/AnimalDb';
 
 function AnimalGame() {
-  // Initialize the random animal directly when the component is loaded
   const randomIndex = Math.floor(Math.random() * animals.length);
   const [randomAnimal] = useState(animals[randomIndex]); // Static random animal
   const [result, setResult] = useState(" "); // Default result text
 
   const handleAnimalClick = (clickedName) => {
-    // Update the result based on the clicked animal
     if (clickedName === randomAnimal.name) {
       setResult('Win');
     } else {
@@ -18,7 +16,6 @@ function AnimalGame() {
   };
 
   const renderAnimalGrid = () => {
-    // Dynamically create rows of 4 animal images each
     const rows = [];
     for (let i = 0; i < animals.length; i += 4) {
       rows.push(
@@ -51,15 +48,12 @@ function AnimalGame() {
         </thead>
         <tbody>
           <tr>
-            {/* Leftmost column: Result */}
             <td className="result-panel">
               <div className={`result ${result.toLowerCase()}`}>{result}</div>
             </td>
-            {/* Second column: Animal Name */}
             <td className="animal-name-panel">
               <div className="animal-name">{randomAnimal.name}</div>
             </td>
-            {/* Right: Animal Grid */}
             <td colSpan="4" className="grid-panel">
               <table className="grid-table">
                 <tbody>{renderAnimalGrid()}</tbody>
